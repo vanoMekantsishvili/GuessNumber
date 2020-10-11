99d8f94b40d3f5b924535c241da26f2a88830084
Vano Mekantsishvili <v.mekantsishvili@gmail.com>
2020-10-11 17:50:39 +0300

    Added input, reset and confirm buttons, added some shadow styles.

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
       <Text>Start a new game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput/>
+        <View style={styles.buttonContainer}>
+          <Button title='Reset' onPress={() => {}}></Button>
+          <Button title='Confirm' onPress={() => {}}></Button>
+        </View>
+      </View>
     </View>
   )
 }
@@ -16,5 +24,24 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center'
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 5, // To display shadow for Android!!!
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
   }
-})
+}) 
